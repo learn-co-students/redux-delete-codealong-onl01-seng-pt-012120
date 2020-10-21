a07d1a8f85da44babde2bd8ef34d5349d7fd7c3d
Charlie Pelton <charliejp0311@gmail.com>
2020-10-21 09:51:57 -0500

    Done.

--- .history/src/reducers/manageTodo_20201021092343.js
+++ .history/src/reducers/manageTodo_20201021092343.js
@@ -3,6 +3,7 @@ import uuid from "uuid";
 export default function manageTodo(state = {
   todos: [],
 }, action) {
+  console.log(action)
   switch (action.type) {
     case 'ADD_TODO':
       const todo = {
@@ -14,7 +15,7 @@ export default function manageTodo(state = {
 
     case 'DELETE_TODO':
 
-      return {todos: state.todos.filter(todo => todo.id !== action.payload)}
+      return {todos: state.todos.filter(todo => todo !== action.payload)}
 
     default:
       return state;
